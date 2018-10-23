c430f455648b4700640e5296d3bbd23c002ecee6
Tania Whiting <tanwhiting@aol.com>
2018-10-23 01:20:56 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS. each do |brand_name|
+    BRANDS.each do |brand_name|
       if  (brand_name != brand) 
          BRANDS << brand
       end
